98876f40b4f41e45324458174ba74249827e7572
jordan-w-timothy <jordan.w.timothy@gmail.com>
2022-05-21 20:44:41 -0400

    Setup Springboot Project

---
+++ testy_app/testy-api/src/main/java/com/testy/testyapi/TestyApiApplication.java
@@ -0,0 +1,13 @@
+package com.testy.testyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestyApiApplication.class, args);
+	}
+
+}
---
+++ testy_app/testy-api/src/test/java/com/testy/testyapi/TestyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testy.testyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

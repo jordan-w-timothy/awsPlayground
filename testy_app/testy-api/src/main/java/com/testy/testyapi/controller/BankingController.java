adda2df2229d8afc9427633e4c5bec974cd9f7b3
jordan-w-timothy <jordan.w.timothy@gmail.com>
2022-05-22 22:53:26 -0400

    Restructure Angular App/ Servers both java/Ang on port 80

--- testy_app/testy-api/src/main/java/com/testy/testyapi/controller/BankingController.java
+++ testy_app/testy-api/src/main/java/com/testy/testyapi/controller/BankingController.java
@@ -8,7 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/bank")
 public class BankingController {
